215286f1df016028a1919099aee161a99da6e8b3
Sandesh Kenjana Ashok <kas.sandesh@gmail.com>
2019-06-25 16:35:07 +0200

    Exercise 5: Problem 2:d Change new master to write and read status from correct address
    Signed-off-by: Sandesh Kenjana Ashok <kas.sandesh@gmail.com>

--- exercise5_src/bus_ca/new_master.cpp
+++ exercise5_src/bus_ca/new_master.cpp
@@ -2,16 +2,20 @@
 
 void New_master::action()
 {
-  unsigned data1, data2, result;
-  wait(500, SC_NS);
-  for( unsigned addr = 0; addr < 16; addr++ )
-  {
-    initiator_port->read(addr, data1, id);
-    initiator_port->read(addr+16, data2, id);
-    initiator_port->write(1024, data1, id);
-    initiator_port->write(1025, data2, id);
-    initiator_port->read(1026, result, id);
-    initiator_port->write(addr+64, result, id);
+
+  wait(1000, SC_NS);
+
+  cout << "master3 writing...." << endl;
+  initiator_port->write(1024, 0, id);
+  initiator_port->write(1025, 16, id);
+  initiator_port->write(1026, 64, id);
+  initiator_port->write(1027, 16, id);
+  initiator_port->write(1028, 1, id);
+
+  cout << "master3 wait for finishing..." <<endl;
+  unsigned status = 1;
+  while(status){
+    wait(10, SC_NS);
+    initiator_port->read(1028, status, id);
   }
-  wait();
 }
